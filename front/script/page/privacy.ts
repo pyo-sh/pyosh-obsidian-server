58eb4a5c98a1a58b0577eb682444172fdc022a3b
pyo-sh <pygosky@gmail.com>
2025-12-20 11:28:27 +0900

    Refactoring Privacy Policy files for terms of service

--- front/script/page/privacy.ts
+++ front/script/page/privacy.ts
@@ -1,7 +1,6 @@
 import "@style/app.css";
-import "@style/warning.css";
 import "@style/footer.css";
-import "@style/privacy.css";
+import "@style/policy.css";
 import "@script/common/navbar";
 import { setupNavbar } from "@script/common/navbar";
 
